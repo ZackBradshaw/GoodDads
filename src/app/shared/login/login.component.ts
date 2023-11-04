7ae841a1ca145b83145efb0b429225d55ae401d5
abem93 <aimejia4@gmail.com>
2023-11-04 17:14:14 -0500

    login page created

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -5,6 +5,7 @@ import { EMPTY, Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { traceUntilFirst } from '@angular/fire/performance';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,46 +13,56 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  private readonly userDisposable: Subscription|undefined;
-  public readonly user: Observable<User | null> = EMPTY;
+  // private readonly userDisposable: Subscription|undefined;
+  // public readonly user: Observable<User | null> = EMPTY;
 
-  showLoginButton = true;
-  showLogoutButton = true;
+  // showLoginButton = true;
+  // showLogoutButton = true;
 
-  userEmail: string = '';
-  userPassword: string = '';
+  // userEmail: string = '';
+  // userPassword: string = '';
 
   constructor(@Optional() private auth: Auth, private router: Router) {
-    if (auth) {
-      this.user = authState(this.auth);
-      this.userDisposable = authState(this.auth).pipe(
-        traceUntilFirst('auth'),
-        map(u => !!u)
-      ).subscribe(isLoggedIn => {
-        this.showLoginButton = !isLoggedIn;
-        this.showLogoutButton = isLoggedIn;
-      });
-    }
+    // if (auth) {
+    //   this.user = authState(this.auth);
+    //   this.userDisposable = authState(this.auth).pipe(
+    //     traceUntilFirst('auth'),
+    //     map(u => !!u)
+    //   ).subscribe(isLoggedIn => {
+    //     this.showLoginButton = !isLoggedIn;
+    //     this.showLogoutButton = isLoggedIn;
+    //   });
+    // }
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+
+   }
 
   ngOnDestroy(): void {
-    if (this.userDisposable) {
-      this.userDisposable.unsubscribe();
-    }
+
+    // if (this.userDisposable) {
+    //   this.userDisposable.unsubscribe();
+    // }
   }
 
   // async loginWithEmailAndPassword() {
   //   return await signInWithEmailAndPassword(this.auth, this.userEmail, this.userPassword);
   // }
 
-  async loginWithGoogle() {
-    const res = await signInWithPopup(this.auth, new GoogleAuthProvider());
-    await console.log(res);
-  }
+  // async loginWithGoogle() {
+  //   const res = await signInWithPopup(this.auth, new GoogleAuthProvider());
+  //   await console.log(res);
+  // }
 
-  async logout() {
-    return await signOut(this.auth);
+  // async logout() {
+  //   return await signOut(this.auth);
+  // }
+
+  signIn(form: NgForm){
+    const email = form.form.value.email
+    if(email === 'admin@test.com'){
+
+    }
   }
 }
