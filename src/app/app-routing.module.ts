a83d29259f71f1facc545b3035177cda031ecdcf
abem93 <aimejia4@gmail.com>
2023-11-04 00:21:34 -0500

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/login/login.component';
+import { AdminWebComponent } from './admin-web/admin-web.component';
+import { CompanionMobileComponent } from './companion-mobile/companion-mobile.component';
+import { HomeComponent } from './companion-mobile/home/home.component';
+import { SupportComponent } from './companion-mobile/support/support.component';
+import { ScheduleComponent } from './companion-mobile/schedule/schedule.component';
+import { MoreComponent } from './companion-mobile/more/more.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '', pathMatch: 'full' },
+  {path: '', component: LoginComponent},
+  {path: 'admin', component: AdminWebComponent},
+  {path: 'user', component: CompanionMobileComponent, children:[
+    {path:'', redirectTo:'/user/home', pathMatch:'full' },
+    {path:'home', component: HomeComponent},
+    {path: 'support', component: SupportComponent},
+    {path: 'schedule', component: ScheduleComponent},
+    {path: 'more', component: MoreComponent}
+  ]},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
